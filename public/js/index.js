7565ab3cb41f1f0c0615eb01c532ecc0894f0864
myusername <hsuresh@mit.edu>
2017-04-16 14:24:08 -0400

    multi bar graph

--- public/js/index.js
+++ public/js/index.js
@@ -17,6 +17,11 @@ var COUNTY_FIPS = {
 }
 
 var INCOME_BRACKETS = "B19001_002E,B19001_003E,B19001_004E,B19001_005E,B19001_006E,B19001_007E,B19001_008E,B19001_009E,B19001_010E,B19001_011E,B19001_012E,B19001_013E,B19001_014E,B19001_015E,B19001_017E";
+var INCOME_LABELS = ["Less than\n10000", "10000 to\n14999", "15000 to\n19999", "20000 to\n24999", "25000 to\n29999", "30000 to\n34999", "35000 to\n39999", "40000 to\n44999", "45000 to\n49999", "50000 to\n59999", "60000 to\n74999", "75000 to\n99999", "100000 to\n124999", "125000 to\n149999", "150000 to\n199999", "Over\n200000"];
+
+function getSum(total, num) {
+    return total + num;
+}
 
 $(document).ready(function () {
 
@@ -40,40 +45,36 @@ $(document).ready(function () {
                 in: "state:" + STATE_FIP,
                 key: KEY
             },
-            "success": function (response) {
-                $.ajax(BASE_URL, {
-                    "method": "GET",
-                    "data": {
-                        get: "NAME," + INCOME_BRACKETS,
-                        for: "county:" + response.county_fip,
-                        in: "state:" + STATE_FIP,
-                        key: KEY
-                    },
-                    "success": function (resp) {
-                        var names = resp[0].slice(1, -2)
-                        var values = resp[1].slice(1, -2)
-                        var labels = names.map(function getLabel(name) {
-                            return INCOME_LABELS.name;
-                        });
+            "success": function (resp) {
+                console.log(resp);
+                console.log(INCOME_LABELS);
+                var data = [];
 
-
-                        console.log(resp)
-                        console.log(labels);
-                        console.log(values);
-
-                        var data = [
-                          {
-                            x: names,
-                            y: values,
-                            type: 'bar'
-                          }
-                        ];
-
-                        Plotly.newPlot('myDiv', data);
+                if ($(".percentages").is(":checked")) {
+                    for (i = 1; i < resp.length; i++) { 
+                        var vals = resp[i].slice(1,-2);
+                        var sum = vals.reduce(getSum)
+                        var percentages = vals.map(function(x) {return x*1.0/sum});
+                        console.log(percentages);
+                        resp[i].slice(1,-2) = percentages;
                     }
-                });
+                }
 
+                console.log(resp);
+
+                for (i = 1; i < resp.length; i++) { 
+                    data.push({
+                        x: INCOME_LABELS,
+                        y: resp[i].slice(1, -2),
+                        name: resp[i][0],
+                        type: 'bar'
+                    });
+                }
+
+                var layout = {barmode: 'group'};
+                Plotly.newPlot('myDiv', data, layout);
             }
         });
+
     });
 });
