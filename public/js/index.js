b759c245497c4a29537718b2177febbc05c0602a
Maryam Archie <marchie@mit.edu>
2017-04-16 20:01:10 -0400

    Retrieve key from json

--- public/js/index.js
+++ public/js/index.js
@@ -2,18 +2,18 @@ var STATE_FIP = "25";
 var BASE_URL = "http://api.census.gov/data/2015/acs1";
 
 var COUNTY_FIPS = {
-    "Barnstable" : "001",
-    "Berkshire" : "003",
-    "Bristol" : "005",
-    "Essex" : "009",
-    "Franklin" : "011",
-    "Hampden" : "013",
-    "Hampshire" : "015",
-    "Middlesex" : "017",
-    "Norfolk" : "021",
-    "Plymouth" : "023",
-    "Suffolk" : "025",
-    "Worcester" : "027"
+    "Barnstable": "001",
+    "Berkshire": "003",
+    "Bristol": "005",
+    "Essex": "009",
+    "Franklin": "011",
+    "Hampden": "013",
+    "Hampshire": "015",
+    "Middlesex": "017",
+    "Norfolk": "021",
+    "Plymouth": "023",
+    "Suffolk": "025",
+    "Worcester": "027"
 }
 
 var INCOME_BRACKETS = "B19001_002E,B19001_003E,B19001_004E,B19001_005E,B19001_006E,B19001_007E,B19001_008E,B19001_009E,B19001_010E,B19001_011E,B19001_012E,B19001_013E,B19001_014E,B19001_015E,B19001_017E";
@@ -37,42 +37,46 @@ $(document).ready(function () {
             countyInput += COUNTY_FIPS[$(this).text()] + ",";
         });
 
-        $.ajax(BASE_URL, {
-            "method": "GET",
-            "data": {
-                get: "NAME," + INCOME_BRACKETS,
-                for: "county:" + countyInput.slice(0,-1),
-                in: "state:" + STATE_FIP,
-                key: KEY
-            },
-            "success": function (resp) {
-                console.log(resp);
-                console.log(INCOME_LABELS);
-                var data = [];
+        $.getJSON("../settings.json", function (json) {
+            // TODO: Retrieve KEY using route
+            $.ajax(BASE_URL, {
+                "method": "GET",
+                "data": {
+                    get: "NAME," + INCOME_BRACKETS,
+                    for: "county:" + countyInput.slice(0, -1),
+                    in: "state:" + STATE_FIP,
+                    key: json.KEY
+                },
+                "success": function (resp) {
+                    console.log(resp);
+                    console.log(INCOME_LABELS);
+                    var data = [];
 
-                if ($(".percentages").is(":checked")) {
-                    for (i = 1; i < resp.length; i++) { 
-                        var vals = resp[i].slice(1,-2);
-                        var sum = vals.reduce(getSum)
-                        var percentages = vals.map(function(x) {return x*1.0/sum});
-                        console.log(percentages);
-                        resp[i].slice(1,-2) = percentages;
+                    if ($(".percentages").is(":checked")) {
+                        for (i = 1; i < resp.length; i++) {
+                            var vals = resp[i].slice(1, -2);
+                            var sum = vals.reduce(getSum)
+                            var percentages = vals.map(function (x) { return x * 1.0 / sum });
+                            console.log(percentages);
+                            resp[i].slice(1, -2) = percentages;
+                        }
                     }
-                }
 
-                for (i = 1; i < resp.length; i++) { 
-                    data.push({
-                        x: INCOME_LABELS,
-                        y: resp[i].slice(1, -2),
-                        name: resp[i][0],
-                        type: 'bar'
-                    });
-                }
+                    for (i = 1; i < resp.length; i++) {
+                        data.push({
+                            x: INCOME_LABELS,
+                            y: resp[i].slice(1, -2),
+                            name: resp[i][0],
+                            type: 'bar'
+                        });
+                    }
 
-                var layout = {barmode: 'group'};
-                Plotly.newPlot('myDiv', data, layout);
-            }
-        });
+                    var layout = { barmode: 'group' };
+                    Plotly.newPlot('myDiv', data, layout);
+                }
+            });
+        })
+
 
     });
 });
